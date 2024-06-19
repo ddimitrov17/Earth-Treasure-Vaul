1c8f955ddb6c0532b080682254b0d6b45dcbca7a
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-06-19 16:01:55 +0300

    render 404

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const { home, dashboard, search, details } = require('../controllers/catalog');
 const { createGET, createPOST } = require('../controllers/stones');
+const { registerGET, loginGET } = require('../controllers/user');
+const { errorPage } = require('../controllers/404');
 
 const router = Router();
 
@@ -9,9 +11,13 @@ router.get('/dashboard', dashboard);
 router.get('/search', search);
 router.get('/create/stone', createGET);
 router.get('/details/:id', details);
+router.get('/register', registerGET);
+router.get('/login', loginGET);
 
 
 
 router.post('/create/stone', createPOST);
 
+
+router.get('*', errorPage);
 module.exports = { router };
